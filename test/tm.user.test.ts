e85b684f969fae145815cef79a560b46288c5623
rdx <reydux95@gmail.com>
2021-08-14 03:34:11 +0200

    ~ Harmonização dos testes.

--- test/tm.user.test.ts
+++ test/tm.user.test.ts
@@ -3,11 +3,11 @@ import { Users } from '../src';
 describe('classe TmUser', () => {
 	const TmUser = new Users().tm;
 
-	it('método Get - Username: luf', () => {
+	it('método getByUsername - Username: luf', () => {
 		expect(TmUser.getByUsername('luf')).resolves.toHaveProperty('id');
 	});
 
-	it('método Get - Username: aqsx26d29r5f9r', () => {
+	it('método getByUsername - Username: aqsx26d29r5f9r', () => {
 		expect(TmUser.getByUsername('aqsx26d29r5f9r')).rejects.toThrow(Error);
 	});
 });

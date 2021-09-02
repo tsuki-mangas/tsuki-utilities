bfd28e0a8e78323bb901453c263dd1a6cf4fb055
rdx <reydux95@gmail.com>
2021-09-02 13:41:25 +0200

    + Exportar tudo que dê.

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,14 @@
-import TmPage from './structures/tm/tm.page';
-import MdPage from './structures/md/md.page';
-import MalPage from './structures/mal/mal.page';
+export { default as TmUser, ReceivedFromApi } from './structures/tm/tm.user';
+export { default as TmPage } from './structures/tm/tm.page';
+export { default as TmScan } from './structures/tm/tm.scan';
 
-import TmUser from './structures/tm/tm.user';
+export { default as MdPage } from './structures/md/md.page';
 
-import TmScan from './structures/tm/tm.scan';
+export { default as MalPage } from './structures/mal/mal.page';
 
-export class Pages {
-	tm: TmPage;
-	md: MdPage;
-	mal: MalPage;
+export { default as ApiRequestError } from './structures/api.request.error';
 
-	constructor() {
-		this.tm = new TmPage();
-		this.md = new MdPage();
-		this.mal = new MalPage();
-	}
-}
-
-export class Users {
-	tm: TmUser;
-
-	constructor() {
-		this.tm = new TmUser();
-	}
-}
-
-export class Scans {
-	tm: TmScan;
-
-	constructor() {
-		this.tm = new TmScan();
-	}
-}
+export * from './types/package.types';
+export * from './types/tm.types';
+export * from './types/md.types';
+export * from './types/mal.types';

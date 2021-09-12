4a0ffa260ded60e712a8e25928b05ed97528cdc5
rdx <reydux95@gmail.com>
2021-09-12 17:00:05 +0200

    + Novo teste na Tsuki Mangás.

--- test/tm.page.test.ts
+++ test/tm.page.test.ts
@@ -9,12 +9,18 @@ describe('classe TmPage', () => {
 			1, // Solo Leveling
 			10 // Domestic na Kanojo
 		],
+		searchQuery = 'Solo Leveling',
 		deletedPagesId = [4, 451, 1113, 1784, 2576];
 
 	it(`método get - Ids: ${workingPagesId.join(', ')}`, (done) => {
 		for (const id of workingPagesId.values())
 			expect(tmPage.get(id)).resolves.toHaveProperty('ids');
 
+		done();
+	});
+
+	it(`método search - Query: ${searchQuery}`, (done) => {
+		expect(tmPage.search(searchQuery)).resolves;
 		done();
 	});
 

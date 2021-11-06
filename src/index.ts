37c82473f65dde4715c28b5d60b9616c4593755a
rdx <reydux95@gmail.com>
2021-11-06 04:57:04 +0100

    ~ Organizar os exports de outra forma.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 export { default as TmPage } from './structures/tm/tm.page';
+export { default as TmUser } from './structures/tm/tm.user';
+export { default as TmChapter } from './structures/tm/tm.chapter';
+export { default as TmScan } from './structures/tm/tm.scan';
+
 export { default as MdPage } from './structures/md/md.page';
 export { default as AlPage } from './structures/al/al.page';
 export { default as MalPage } from './structures/mal/mal.page';
 
-export { default as MalPageSearch } from './structures/mal/mal.page.search';
-
-export { default as TmChapter } from './structures/tm/tm.chapter';
-export { default as TmScan } from './structures/tm/tm.scan';
-export { default as TmUser } from './structures/tm/tm.user';
+// export { default as MalPageSearch } from './structures/mal/mal.page.search';
 
 export { default as ApiRequestError } from './structures/api.request.error';
 
-export * from './helpers/array';
 export * from './helpers/string';
+export * from './helpers/array';
 export * from './helpers/number';
 export * from './helpers/check';
 
